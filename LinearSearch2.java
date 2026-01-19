8cfddd5c8a332a1afc8b21bde6f7a425c418895b
luckysharma5609 <your_github_email@example.com>
2026-01-19 17:55:25 +0400

    Updated linear search implementation and added arrays examples

---
+++ LinearSearch2.java
@@ -0,0 +1,38 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        String name = "Akash";
+        char target = 'm';
+        System.out.println(search(name , target));
+
+        
+    }
+
+    static boolean search(String str , char target){
+        if(str.length() == 0){
+            return false;
+        }
+
+        for(int i = 0; i<str.length(); i++ ){
+            if(target == str.charAt(i)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    //static int linear(int[] arr, int target){
+    //    if(arr.length == 0){
+     //       return -1;
+     //   }
+       // for(int index = 0; index < arr.length; index++){
+      //      int element = arr[index];
+        //    if(element == target){
+          //      return index;
+            //}
+        //}
+     //   return -1;
+        
+
+
+}
